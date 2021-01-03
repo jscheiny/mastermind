1138be0c25aea3e02e0d845025534afba3e2c563
Jonah Scheinerman <jonah@scheinerman.net>
2021-01-03 12:07:11 -0500

    compare_evaluators

--- src/evaluators.hpp
+++ src/evaluators.hpp
@@ -1,9 +1,23 @@
 #ifndef EVALUATORS_HPP
 #define EVALUATORS_HPP
 
+#include "console.hpp"
+#include <iostream>
 #include <vector>
 
 auto average_narrowing(int guess, const std::vector<int>& search_space) -> int;
 auto max_partition_size(int guess, const std::vector<int>& search_space) -> int;
+
+template<typename A, typename B>
+auto compare_evaluators(A evaluator_a, B evaluator_b) {
+    return [evaluator_a, evaluator_b](int guess, const std::vector<int>& search_space) -> int {
+        int result_a = evaluator_a(guess, search_space);
+        int result_b = evaluator_b(guess, search_space);
+        if (result_a != result_b) {
+            std::cout << "Evaluators are not the same "<< std::endl;
+        }
+        return result_a;
+    };
+}
 
 #endif
