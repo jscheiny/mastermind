a5b05994eb3aa17adbb19e82766309e42ee3448b
jscheiny <jonah@scheinerman.net>
2021-01-03 16:47:36 -0500

    Refactor guessers

--- src/parallelized_guesser.hpp
+++ src/parallelized_guesser.hpp
@@ -9,89 +9,79 @@
 #include <functional>
 #include <future>
 
-template<typename E>
-class parallelized_guesser {
-public:
-    parallelized_guesser(
-        E evaluate_guess,
-        int threads = std::thread::hardware_concurrency()
-    ): evaluate_guess_{evaluate_guess}, threads_{threads} {}
-
-    auto operator()(const std::vector<int>& guess_history, const std::vector<int>& search_space) const -> int;
-
-private:
-    E evaluate_guess_;
-    const int threads_;
-
+namespace impl {
     struct best_guess {
         int guess;
-        int metric;
+        int score;
     };
 
-    auto find_best_guess(const std::vector<int>& search_space, int offset) const -> best_guess;
-};
-
-template<typename E>
-auto make_parallelized_guesser(E evaluator) -> parallelized_guesser<E> {
-    return {evaluator};
+    template<typename S>
+    auto find_best_guess(const std::vector<int>& search_space, const S& score_fn, int threads, int offset) -> best_guess;
 }
 
-template<typename E>
-auto parallelized_guesser<E>::operator()(
-    const std::vector<int>& guess_history,
-    const std::vector<int>& search_space
-) const -> int {
-    if (guess_history.empty()) {
-        return random::index(match_table::MAX_GUESS);
-    }
+template<typename S>
+auto make_parallelized_guesser(
+    const S& score_fn,
+    int threads = std::thread::hardware_concurrency()
+) {
+    return [&score_fn, threads](const std::vector<int>& guess_history, const std::vector<int>& search_space) -> int {
+        if (guess_history.empty()) {
+            return random::index(match_table::MAX_GUESS);
+        }
 
-    if (search_space.size() == 1) {
-        return search_space[0];
-    }
+        if (search_space.size() == 1) {
+            return search_space[0];
+        }
 
-    if (search_space.size() < threads_ || threads_ == 1) {
-        return find_best_guess(search_space, 0).guess;
-    }
+        if (search_space.size() < threads || threads == 1) {
+            return impl::find_best_guess(search_space, score_fn, 1, 0).guess;
+        }
 
-    std::vector<std::future<best_guess>> futures;
+        std::vector<std::future<impl::best_guess>> futures;
+        for (int offset = 0; offset < threads; offset++) {
+            futures.push_back(
+                std::async(std::launch::async, [&search_space, &score_fn, threads, offset]() {
+                    return impl::find_best_guess(search_space, score_fn, threads, offset);
+                })
+            );
+        }
 
-    for (int offset = 0; offset < threads_; offset++) {
-        futures.push_back(
-            std::async(
-                std::launch::async, 
-                [this, offset, &search_space]() { return find_best_guess(search_space, offset); }
-            )
-        );
-    }
+        std::vector<impl::best_guess> results;
+        for (auto& future : futures) {
+            results.push_back(future.get());
+        }
 
-    std::vector<best_guess> results;
-    for (auto& future : futures) {
-        results.push_back(future.get());
-    }
-
-    random::shuffle(results.begin(), results.end());
-    auto min = std::min_element(results.begin(), results.end(), [](best_guess a, best_guess b) { return a.metric < b.metric; });
-    return min->guess;
+        random::shuffle(results.begin(), results.end());
+        auto min = std::min_element(results.begin(), results.end(), [](impl::best_guess a, impl::best_guess b) {
+            return a.score < b.score;
+        });
+        return min->guess;
+    };
 }
 
-template<typename E>
-auto parallelized_guesser<E>::find_best_guess(const std::vector<int>& search_space, int offset) const -> best_guess {
-    int min_metric = std::numeric_limits<int>::max();
+template<typename S>
+auto make_simple_guesser(const S& score_fn) {
+    return make_parallelized_guesser(score_fn, 1);
+}
+
+template<typename S>
+auto impl::find_best_guess(const std::vector<int>& search_space, const S& score_fn, int threads, int offset) -> best_guess {
+    int min_score = std::numeric_limits<int>::max();
     std::vector<int> min_guesses;
-    for (int index = offset; index < search_space.size(); index += threads_) {
+    for (int index = offset; index < search_space.size(); index += threads) {
         int guess = search_space[index];
-        int metric = evaluate_guess_(guess, search_space);
-        if (metric < min_metric) {
-            min_metric = metric;
+        int score = score_fn(guess, search_space);
+        if (score < min_score) {
+            min_score = score;
             min_guesses.clear();
             min_guesses.push_back(guess);
-        } else if (metric == min_metric) {
+        } else if (score == min_score) {
             min_guesses.push_back(guess);
         }
     }
 
     int random_index = random::index(min_guesses.size());
-    return {.guess = min_guesses[random_index], .metric = min_metric};
+    return { .guess = min_guesses[random_index], .score = min_score };
 }
 
 #endif
