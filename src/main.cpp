1ebf19d00a4c2721508cbc618707902fc450b44c
Jonah Scheinerman <jonah@scheinerman.net>
2021-01-04 20:10:52 -0500

    Move guessing up

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,8 @@
 #include <sstream>
 #include <stdlib.h>
 
-template<typename E>
-auto simulate_games(const std::string& name, int games, E evaluator) -> void {
-    auto guesser = make_parallelized_guesser(evaluator);
-    std::vector<int> move_counts;
-    {
-        std::stringstream timer_name;
-        timer_name << games << " " << name << " games";
-        scoped_timer timer{timer_name.str()};
-        for (int i = 0; i < games; i++) {
-            int moves = play_mastermind(guesser, random_secret());
-            move_counts.push_back(moves);
-        }
-        std::cout << std::endl;   
-    }
-
-    int sum = std::accumulate(move_counts.begin(), move_counts.end(), 0);
-    double n = (double) games;
-    double mean = sum / n;
-    double total_deviation = 0;
-    for (int count : move_counts) {
-        int deviation = count - mean;
-        total_deviation += deviation * deviation;
-    }
-    double variance = total_deviation / n;
-    double stddev = sqrt(variance);
-
-    std::cout << "Average moves = " << console::green_fg << mean << console::reset << std::endl;
-    std::cout << "Standard deviation = " << console::green_fg << stddev << console::reset << std::endl;
-}
+template<typename S>
+auto simulate_games(int games, const S& score_fn, int threads = std::thread::hardware_concurrency()) -> void;
 
 auto main(int argc, char* argv[]) -> int {
     if (argc < 1) {
@@ -52,6 +25,33 @@ auto main(int argc, char* argv[]) -> int {
 
     match_table::initialize();
 
-    simulate_games("average block size", games, average_block_size);
-    simulate_games("max block size", games, max_block_size);
+    simulate_games(games, average_block_size);
+}
+
+template<typename S>
+auto simulate_games(int games, const S& score_fn, int threads) -> void {
+    scoped_timer timer{"Games"};
+    std::vector<std::future<int>> futures;
+    int games_per_thread = games / threads;
+    for (int i = 0; i < threads; i++) {
+        futures.push_back(
+            std::async(std::launch::async, [&score_fn, games_per_thread, threads]() {
+                auto guesser = make_guesser(score_fn);
+                int total_moves = 0;
+                for (int i = 0; i < games_per_thread; i++) {
+                    total_moves += play_mastermind(guesser, random_secret());
+                }
+                return total_moves;
+            })
+        );
+    }
+
+    int total_moves = 0;
+    for (auto& future : futures) {
+        total_moves += future.get();
+    }
+
+    double n = (double) games;
+    double mean = total_moves / n;
+    std::cout << "Average moves = " << console::green_fg << mean << console::reset << std::endl;
 }
--- src/parallelized_guesser.hpp
+++ src/parallelized_guesser.hpp
@@ -9,18 +9,8 @@
 #include <functional>
 #include <future>
 
-namespace impl {
-    struct best_guess {
-        int guess;
-        int score;
-    };
-
-    template<typename S>
-    auto find_best_guess(const std::vector<int>& search_space, const S& score_fn, int threads, int offset) -> best_guess;
-}
-
 template<typename S>
-auto make_parallelized_guesser(
+auto make_guesser(
     const S& score_fn,
     int threads = std::thread::hardware_concurrency()
 ) {
@@ -33,55 +23,22 @@ auto make_parallelized_guesser(
             return search_space[0];
         }
 
-        if (search_space.size() < threads || threads == 1) {
-            return impl::find_best_guess(search_space, score_fn, 1, 0).guess;
+        int min_score = std::numeric_limits<int>::max();
+        std::vector<int> min_score_guesses;
+        for (int guess : search_space) {
+            int score = score_fn(guess, search_space);
+            if (score < min_score) {
+                min_score = score;
+                min_score_guesses.clear();
+                min_score_guesses.push_back(guess);
+            } else if (score == min_score) {
+                min_score_guesses.push_back(guess);
+            }
         }
 
-        std::vector<std::future<impl::best_guess>> futures;
-        for (int offset = 0; offset < threads; offset++) {
-            futures.push_back(
-                std::async(std::launch::async, [&search_space, &score_fn, threads, offset]() {
-                    return impl::find_best_guess(search_space, score_fn, threads, offset);
-                })
-            );
-        }
-
-        std::vector<impl::best_guess> results;
-        for (auto& future : futures) {
-            results.push_back(future.get());
-        }
-
-        random::shuffle(results.begin(), results.end());
-        auto min = std::min_element(results.begin(), results.end(), [](impl::best_guess a, impl::best_guess b) {
-            return a.score < b.score;
-        });
-        return min->guess;
+        int random_index = random::index(min_score_guesses.size());
+        return min_score_guesses[random_index];
     };
-}
-
-template<typename S>
-auto make_simple_guesser(const S& score_fn) {
-    return make_parallelized_guesser(score_fn, 1);
-}
-
-template<typename S>
-auto impl::find_best_guess(const std::vector<int>& search_space, const S& score_fn, int threads, int offset) -> best_guess {
-    int min_score = std::numeric_limits<int>::max();
-    std::vector<int> min_guesses;
-    for (int index = offset; index < search_space.size(); index += threads) {
-        int guess = search_space[index];
-        int score = score_fn(guess, search_space);
-        if (score < min_score) {
-            min_score = score;
-            min_guesses.clear();
-            min_guesses.push_back(guess);
-        } else if (score == min_score) {
-            min_guesses.push_back(guess);
-        }
-    }
-
-    int random_index = random::index(min_guesses.size());
-    return { .guess = min_guesses[random_index], .score = min_score };
 }
 
 #endif
