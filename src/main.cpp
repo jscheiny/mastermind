d421666f725a4c55b67096e15227db97cda6e1de
jscheiny <jonah@scheinerman.net>
2021-01-10 10:24:48 -0500

    Fix denominator

--- src/main.cpp
+++ src/main.cpp
@@ -29,25 +29,27 @@ auto simulate_games(int games, const S& score_fn, int threads) -> void {
     scoped_timer timer{"Games"};
     std::vector<std::future<int>> futures;
     int games_per_thread = games / threads;
-    for (int i = 0; i < threads; i++) {
+    int mod = games % threads;
+    for (int thread = 0; thread < threads; thread++) {
         futures.push_back(
-            std::async(std::launch::async, [&score_fn, games_per_thread, threads]() {
+            std::async(std::launch::async, [&score_fn, games_per_thread, mod, thread, threads]() {
                 auto guesser = make_scored_guesser(score_fn);
-                int total_moves = 0;
+                int moves = 0;
+                int games = thread < mod ? games_per_thread + 1 : games_per_thread;
                 for (int i = 0; i < games_per_thread; i++) {
-                    total_moves += play_mastermind(guesser, random_secret());
+                    moves += play_mastermind(guesser, random_secret());
                 }
-                return total_moves;
+                return moves;
             })
         );
     }
 
+    double total_games = (double) games;
     int total_moves = 0;
     for (auto& future : futures) {
         total_moves += future.get();
     }
 
-    double n = (double) games;
-    double mean = total_moves / n;
+    double mean = total_moves / total_games;
     std::cout << "Average moves = " << console::green_fg << mean << console::reset << std::endl;
 }
